fd90e9d4b53fa0b66ae40b6566de4b95ef5d1057
Neovodoo <75006634+Neovodoo@users.noreply.github.com>
2024-10-13 22:52:23 +0300

    Add logs for Chat controller

--- back/src/main/java/com/Linguatalk/back/controller/ChatController.java
+++ back/src/main/java/com/Linguatalk/back/controller/ChatController.java
@@ -3,6 +3,8 @@ package com.Linguatalk.back.controller;
 import com.Linguatalk.back.model.ChatMessage;
 import com.Linguatalk.back.repository.ChatMessageRepository;
 import com.Linguatalk.back.service.TranslationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
     @Autowired
     private TranslationService translationService;
 
@@ -20,6 +24,7 @@ public class ChatController {
 
     @PostMapping("/translate")
     public ChatMessage translateChatMessage(@RequestBody ChatMessage chatMessage) {
+        logger.info("Received request to translate message: {}", chatMessage);
         String translatedText = translationService.translateMessage(chatMessage.getMessage(), chatMessage.getLanguage());
         chatMessage.setTranslatedMessage(translatedText);
         return chatMessageRepository.save(chatMessage);
