5ec15586b58e3afc60a9605fae1598c0da2a19e5
Кирилл <r.k.a.77@mail.ru>
2024-10-20 01:49:54 +0300

    Downloading models option

--- back/src/main/java/com/Linguatalk/back/BackMobApplication.java
+++ back/src/main/java/com/Linguatalk/back/BackMobApplication.java
@@ -1,5 +1,6 @@
 package com.Linguatalk.back;
 
+import com.Linguatalk.back.service.TranslationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,11 @@ public class BackMobApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackMobApplication.class, args);
 		System.out.println("Приложение работает");
+		System.out.println("Начало загрузки моделей ru/en en/ru");
+		TranslationService test = new TranslationService();
+		test.translateMessage("download", "en", "ru");
+		test.translateMessage("загрузить", "ru", "en");
+		System.out.println("Модели ru/en en/ru были загружены");
 	}
 
 }
